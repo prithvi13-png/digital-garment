4c6fe43668652074df2edf2e895b3cae0c185605
prithvi13-png <prithviprabhakarh13@gmail.com>
2026-04-09 17:59:37 +0530

    changes

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from apps.core.views import HealthCheckAPIView
+
 urlpatterns = [
+    path("", HealthCheckAPIView.as_view(), name="root_health_check"),
     path("admin/", admin.site.urls),
     path("api/v1/", include("apps.api_urls")),
 ]
